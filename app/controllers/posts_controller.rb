7ccd1972800912e070f37ad3b75dc1114bf44486
neovrnaz <neovrnaz@gmail.com>
2020-10-24 00:22:20 -0600

    Add a body column to the database

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,11 @@
 class PostsController < ApplicationController
-  before_action :set_post, only: [:show, :edit, :update, :destroy]
+  before_action :set_post, only: %i[show edit update destroy]
 
   # GET /posts
   # GET /posts.json
   def index
     @posts = Post.all
+    render template: 'posts/index.html.erb', layout: 'application'
   end
 
   # GET /posts/1
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_23_051817) do
+ActiveRecord::Schema.define(version: 2020_10_24_055237) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.text "body"
   end
 
 end
