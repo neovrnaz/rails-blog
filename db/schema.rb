6083933fb65a109bf536f4270a5de521bace3161
neovrnaz <neovrnaz@gmail.com>
2020-10-25 19:51:12 -0600

    Add page for viewing blog posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_24_055237) do
+ActiveRecord::Schema.define(version: 2020_10_26_002043) do
+
+  create_table "pages", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.string "slug"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
